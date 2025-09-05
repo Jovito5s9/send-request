a283b8a10be1af63ebd67da902c62e511b171131
Jovito5s9 <sjovito70@gmail.com>
2025-09-05 11:06:52 -0300

    Servidor agora pode enviar mensagens para o cliente

--- serverTCP.py
+++ serverTCP.py
@@ -4,25 +4,38 @@ import threading
 bind_ip = 'localhost'  #nome do server
 bind_port = 8080       #porta do meu server
 
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)#AF_NET=IP V6//SOCK_STREAM=protocolo TCP
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)#AF_NET=IP V6//SOCK_STREAM=protocolo TCP, juntos sao TCP/IP
 server.bind((bind_ip,bind_port))
 server.listen(5)
 print ('[*] Escutando %s:%d ' %(bind_ip,bind_port))
 
-def handle_client(client_socket):
+def handle_recive(client_socket,addr):
     while True:
         try:
             request = client_socket.recv(1024)
             if not request:
                 break
             print(f'{addr} disse: %s' %request.decode())
-            client_socket.send((f'Servidor recebeu: {request.decode()}\n').encode())
+            client_socket.send((f'\nServidor recebeu: {request.decode()}\n').encode())
         except:
             break
     client_socket.close()
 
+def handle_send(client_socket,addr):
+    while True:
+        try:
+            mensage=input("[Servidor]: ")
+            client_socket.send((f"[servidor]: {mensage.decode()}").encode())
+        except:
+            pass
+
+def handle_client(client_socket,addr):
+    threading.Thread(target=handle_recive,args=(client_socket,addr)).start()
+    threading.Thread(target=handle_send,args=(client_socket,addr)).start()
+    pass
+
 while True:
     client, addr= server.accept()
     print('[*] Conexão aceita de: %s:%d' %(addr[0],addr[1]))
-    client_handler= threading.Thread(target=handle_client,args=(client,))
+    client_handler= threading.Thread(target=handle_client,args=(client,addr))
     client_handler.start()
